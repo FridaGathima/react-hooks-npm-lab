265aee7ba07c111f001a8692ffc6e48a6232333e
+ <+>
2023-07-18 11:52:32 +0300

    Completed assignment

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,5 +1,6 @@
 //No need to do any work here.
 //If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
+// console.log("dfghhgfdfdgh");
 import format from "./node_modules/date-fns/esm/format/index.js";
 
 function displayTime() {
